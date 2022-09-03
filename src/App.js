c5d25cf4e88b53f17ed47044679a0d5767c66917
Bryan <bryanparisot@hotmail.com>
2022-09-03 13:10:07 +0200

    :sparkles: Adding first version for work on projet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Index from "./pages/Index.js";
+import NotFound from "./pages/NotFound.js";
+import SignIn from "./pages/SignIn.js";
+import User from "./pages/User.js";
+
+
 function App() {
   return (
-    <div>Test ici</div>
+    <Router>
+      <Switch>
+        <Route exact component={Index} path="/" />
+        <Route exact component={SignIn} path="/sign-in" />
+        <Route exact component={User} path="/user" />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navigations/NavigationConnect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationConnect = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default NavigationConnect;
